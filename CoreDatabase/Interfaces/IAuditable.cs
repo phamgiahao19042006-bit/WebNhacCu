4331b0ada5defefa4c18f8e12b1e1d10e674054d
D24CNA05467 - Thái Minh Dương <duong.d24cna05467@ktkt.edu.vn>
2026-08-03 09:28:04 +0700

    Interfaces

---
+++ CoreDatabase/Interfaces/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreDatabase.Interfaces
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
---
+++ CoreDatabase/Interfaces/IMeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreDatabase.Interfaces
+{
+    public interface IMeta
+    {
+        public string MetaTitle { get; set; }
+        public string MetaKeyword { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
